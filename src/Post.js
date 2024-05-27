c60a8bf9b89207ec86cdf950b064245f6220085e
pamchArch <119898784+pamchiis@users.noreply.github.com>
2024-05-27 21:31:19 +0000

    swap post placeholders with variables

--- src/Post.js
+++ src/Post.js
@@ -14,18 +14,18 @@ function Post({ profileImg, image, username, timestamp, message }) {
 
   return <div className="post">
     <div className="post__top">
-        <Avatar />
+        <Avatar src={profileImg} className="post__avatar"/>
         <div className="post__header">
-            <h4>pamchDev</h4>
+            <h4>{username}</h4>
             <div className="header__timeblock">
-                <h5>11:40PM</h5>
+                <h5>{timestamp}</h5>
                 <CalendarMonthIcon />
             </div>
         </div>
     </div>
     <div className="post__body">
-        <p>Hello World! ^_^</p>
-        <img onClick={handleOpen} alt="" src="https://64.media.tumblr.com/5c7e929e12d0a87c065473a031770e4f/d0d392315e6e5fe8-93/s400x600/09113c326cc63411e974caa0048d00f4239a7ddc.jpg" />
+        <p>{message}</p>
+        <img onClick={handleOpen} alt="" src={image} />
     </div>
     
     <div className="post__bottom">
@@ -45,7 +45,7 @@ function Post({ profileImg, image, username, timestamp, message }) {
 
     <div className="modal__container">
         <Modal open={open} onClose={handleClose} style={{display: 'flex', alignItems:'center', justifyContent:'center'}}>
-                <img className="modalPreview" alt="" src="https://64.media.tumblr.com/5c7e929e12d0a87c065473a031770e4f/d0d392315e6e5fe8-93/s400x600/09113c326cc63411e974caa0048d00f4239a7ddc.jpg" />
+                <img className="modalPreview" alt="" src={image} />
         </Modal>
     </div>
     
