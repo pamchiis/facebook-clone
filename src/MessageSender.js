f4f894fbc3a87f3bbd937202b134d624b5b15524
pamchiis <119898784+pamchiis@users.noreply.github.com>
2024-05-28 22:11:35 -0400

    Revert "make mobile compatible"
    This reverts commit 79f6d640d5be6f0d7ef09b8e071f4e1486e4e84b.

--- src/MessageSender.js
+++ src/MessageSender.js
@@ -14,7 +14,6 @@ function isBlank(str) {
 };
 
 function MessageSender() {
-
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
@@ -52,13 +51,11 @@ function MessageSender() {
   return <div className="messageSender">
 
     <div className="sender__top">
-      <form>
-        <Avatar src={user.photoURL}/>
-        <input value={input} onChange={e => setInput(e.target.value)} type="text" placeholder={`What's on your mind, ${user.displayName}?`}/>
-        <button onClick={handleSubmit} type="submit">
-              <SendIcon />
-        </button>
-      </form>
+      <Avatar src={user.photoURL}/>
+      <input value={input} onChange={e => setInput(e.target.value)} type="text" placeholder={`What's on your mind, ${user.displayName}?`}/>
+      <button onClick={handleSubmit} type="submit">
+            <SendIcon />
+      </button>
     </div>
     <div className="sender__body">
       <img src={imgInput}/>
@@ -69,9 +66,9 @@ function MessageSender() {
             <h4>Live Video</h4>
         </div>
         
-        <div className="sender__option" onClick={handleOpen}>
+        <div className="sender__option">
             <CollectionsIcon />
-            <h4>Photo/Video</h4>
+            <h4 onClick={handleOpen}>Photo/Video</h4>
         </div>
         
         <div className="sender__option">
--- src/Sidebar.js
+++ src/Sidebar.js
@@ -13,12 +13,6 @@ function Sidebar() {
         <SidebarRow src="https://pm1.aminoapps.com/6383/9de094731d26b08748b8e7b9c89f9db9bc100161_00.jpg" title="Frederick Betancourt" />
         <SidebarRow src="https://external-preview.redd.it/kg7PLSFo9qiybvu-uvzqisUBUwJd_UJ4qp1Vxv2uuZw.jpg?width=640&crop=smart&auto=webp&s=7c5c602b39e6b2c2935e84664e468d1431a6c2f0" title="Michael Schmidt" />
         <SidebarRow src="https://i.pinimg.com/474x/9e/e1/c6/9ee1c6f0820cc3799b79f5ae8a464d17.jpg" title="Diego Brandon" />
-        <SidebarRow src="https://pbs.twimg.com/media/GB8XPN2WAAAWw5S?format=jpg&name=large" title="Jerm"/>
-        <SidebarRow src="https://m.media-amazon.com/images/M/MV5BOGI0ODdiNWUtNmY1Yy00YmE0LTlhZTUtZWZhOTU0NDJhOWExXkEyXkFqcGdeQXVyNjgxNDgxMjQ@._V1_.jpg" title="Jabear"/>
-        <SidebarRow src="https://meowden.com/cdn/shop/products/strawberry-hat-for-cats-833.jpg?v=1679324696" title="Joumch"/>
-        <SidebarRow src="https://cdn.discordapp.com/attachments/1130748390574133280/1245123660965220504/image.png?ex=66579b46&is=665649c6&hm=fe8e9a7fd1b23927dcf88acaf49c4af628e2fbf648236b94585794110d1c3dc1&" title="Adamsitou"/>
-        <SidebarRow src="https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcTBCfaBiUsv9SMOHP5U6_S3BOvnEXRdojIthh9YA7wBUIJPq8-u" title="Aclis"/>
-        <SidebarRow src="https://i.imgflip.com/2/8n91ax.jpg" title="Parsley"/>
         <div className="sidebar__footer">
           <h4>See More</h4>
           <KeyboardArrowDownIcon />
